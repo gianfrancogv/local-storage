79f2016fd589e9f0d1bd6c44eb12eca8ae04aeb5
Gian Franco <gianfrancogv@gmail.com>
2020-03-29 20:17:08 -0300

    Cargando tweets del local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,11 +8,14 @@ function eventListeners() {
     document.querySelector('#formulario').addEventListener('submit', agregarTweet);
     //Borrar tweets
     listaTweets.addEventListener('click', borrarTweet);
+    //Contenido cargado
+    document.addEventListener('DOMContentLoaded', localStorageListo);
 }
 
 
 //Functions
 
+//Agregar Tweets
 function agregarTweet(e) {
     e.preventDefault();
     //Leer el valor del text area
@@ -28,12 +31,56 @@ function agregarTweet(e) {
     li.appendChild(botonBorrar);
     //Añade el Tweet a la lista
     listaTweets.appendChild(li);
+    //Añadir Tweet al Local Storage
+    addLocalStorage(tweet);
 }
 
+//Elimina tweets del DOM
 function borrarTweet (e) {
     e.preventDefault();
     if (e.target.className === 'borrar-tweet') {
         e.target.parentElement.remove();
         alert('Tweet eliminado');
     }
+}
+
+//Agrega Tweets al Local Storage
+function addLocalStorage(tweet) {
+    let tweets;
+    tweets = obtenerTweets();
+    //Añadir el nuevo Tweet
+    tweets.push(tweet);
+    //Convertir de string a arreglo para el Local Storage
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+//Comprobar que haya elementos en local storage, retorna un arreglo
+function obtenerTweets() {
+    let tweets;
+    //Revisamos valores de Local Storage
+    if (localStorage.getItem('tweets') === null) {
+        tweets = [];
+    } else {
+        tweets = JSON.parse(localStorage.getItem('tweets'));
+    }
+    return tweets;
+}
+
+//Mostrar datos de Local Storage en la lista
+function localStorageListo() {
+    let tweets;
+    tweets = obtenerTweets();
+    tweets.forEach(function(tweet){
+        //Crear botón de eliminar
+        const botonBorrar = document.createElement('a');
+        botonBorrar.classList = 'borrar-tweet';
+        botonBorrar.innerText = 'X';
+        //Crear elemento y añadirle el contenido a la lista
+        const li = document.createElement('li');
+        li.innerText = tweet;
+        //Añade el botón de borrar al tweet
+        li.appendChild(botonBorrar);
+        //Añade el Tweet a la lista
+        listaTweets.appendChild(li);
+    });
 }
